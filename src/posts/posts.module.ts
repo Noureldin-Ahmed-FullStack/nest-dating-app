a174ad8837c3ae1d49b7a7b15908b5033480e931
noureldin <noureldin2662002@gmail.com>
2026-04-24 20:12:06 +0300

    removing redis module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [RedisModule], // ✅ THIS IS THE FIX
   controllers: [PostsController],
   providers: [PostsService],
 })
